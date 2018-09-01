aa199ac308bb2999c6eff129e3b76dd8e84fc5d4
Turbotwins <Turbotwins@Turbolap>
2018-09-01 16:25:56 +0300

    My first program

---
+++ Java9AndreiDan/src/java9andreidan/Java9AndreiDan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java9andreidan;
+
+/**
+ *
+ * @author Turbotwins
+ */
+public class Java9AndreiDan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Salut distinsi colegi!");// TODO code application logic here
+    }
+    
+}
